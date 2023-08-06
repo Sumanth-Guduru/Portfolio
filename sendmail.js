f27ff907a9c6547d86bf3ebf92990c009b76b3b8
Sumanth <60640082+Sumanth-Guduru@users.noreply.github.com>
2023-08-06 15:29:12 -0400

    Update sendmail.js
    changed code using email.js to send mail

--- sendmail.js
+++ sendmail.js
@@ -1,26 +1,29 @@
+
 function sendMail() {
-  // var email = document.getElementById("input_email").value;
-  // var emailFormat =
-  //   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-  // var error_msg = document.getElementById("error_msg");
-  // if (email.match(emailFormat)) {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "gudurusumanthwgl@gmail.com",
-    Password: "Sumanthreddy",
-    To: "gudurusumanthwgl@gmail.com",
-    From: document.getElementById("input_email").value,
-    Subject: document.getElementById("sub").value,
-    Body: document.getElementById("msg").value,
-  }).then((message) => alert("Message Sent Successfully!!"));
-  var form = document.getElementById("contact-form");
+  //alert("hit");
+  var email = document.getElementById("input_email").value;
+  var emailFormat =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  var error_msg = document.getElementById("error_msg");
 
-  error_msg.innerHTML = "";
-  document.getElementById("contact-form").reset();
-  // } else {
-  //   text = "please enter valid mail";
-  //   error_msg.innerHTML = text;
-  //   error_msg.style.color = "#ff0000";
-  // }
+  if (!email.match(emailFormat)) {
+    error_msg.innerHTML = "Please enter a valid email";
+    error_msg.style.color = "#ff0000";
+    return;
+  }
+  var params={
+  from_name: document.getElementById("name").value,
+  subject: document.getElementById("sub").value,
+  email: email,
+  message: document.getElementById("msg").value,
+  }
+
+  emailjs.send("service_aje3frd","template_kxzmr18",params).then(function(res){
+    alert("Sucess!"+ res.status);
+   
+    })
+
+
 }
+
